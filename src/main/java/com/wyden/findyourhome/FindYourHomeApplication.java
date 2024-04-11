77c5797b58056cf6c024653a678951a75851260d
Jailson Anjos <jailsonanjos@hotmail.com>
2024-04-11 19:04:42 -0300

    feat(): Set up project

---
+++ src/main/java/com/wyden/findyourhome/FindYourHomeApplication.java
@@ -0,0 +1,13 @@
+package com.wyden.findyourhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindYourHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindYourHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyden/findyourhome/FindYourHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyden.findyourhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
