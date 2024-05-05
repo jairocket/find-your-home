a50d4d955f5de5db162f02a409dc5d856cd98adf
Everton Mutti <evertoncastro19@gmail.com>
2024-05-05 19:46:05 -0300

    test merge

--- src/main/java/com/wyden/findyourhome/controllers/IndividualCostumerController.java
+++ src/main/java/com/wyden/findyourhome/controllers/IndividualCostumerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import com.wyden.findyourhome.dto.UpdateCustomerDTO;
 import com.wyden.findyourhome.dto.UpdateTelephoneDTO;
 
+
 import java.util.List;
 import java.net.URI;
 
