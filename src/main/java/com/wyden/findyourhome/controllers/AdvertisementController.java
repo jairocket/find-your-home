b4ce74a9d474f7f58d616333fb1728fe0c9ae6df
gustavo <gl8560012@gmail.com>
2024-05-18 16:08:46 -0300

    Find all Advertisement

--- src/main/java/com/wyden/findyourhome/controllers/AdvertisementController.java
+++ src/main/java/com/wyden/findyourhome/controllers/AdvertisementController.java
@@ -8,7 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wyden.findyourhome.entities.Advertisement;
+
 import com.wyden.findyourhome.services.AdvertisementService;
+
+import java.util.List;
+
+
+
 
 @RestController
 @RequestMapping(value = "/advertisements")
@@ -24,4 +30,9 @@ public class AdvertisementController {
         return ResponseEntity.ok().body(advertisement);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Advertisement>> findAll() {
+       java.util.List<Advertisement> advertisement = service.findAll();
+        return ResponseEntity.ok().body(advertisement);
+    }
 }
